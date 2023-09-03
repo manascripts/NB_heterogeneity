665d68d9227fcf07fda3b63fac56cb30af3271c3
Manas-Sehgal <143714938+Manas-Sehgal@users.noreply.github.com>
2023-09-04 03:01:24 +0530

    Add files via upload

---
+++ GSEA_PCA/GSEA.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from matplotlib.pyplot import figure
+import gseapy as gp
+
+df=pd.read_csv('./Data/GSEID.txt', sep= " ",header=None).transpose()
+df.columns=df.iloc[0]
+df=df.drop([0])
+df=df.fillna(0)
+X_std = StandardScaler().fit_transform(df)
+pca = PCA(n_components = 4)
+principalComponents = pca.fit_transform(X_std)
+PC = pd.DataFrame(principalComponents,columns = ["PC" + str(x+1) for x in range(4)])
+model = KMeans(n_clusters=2).fit(df)
+
+cutoff = 10; #cutoff is where you set the classification
+cls=[]
+for i in range(len(PC['PC1'])):
+    if PC['PC1'][i] < cutoff:
+        print('NOR')
+        cls.append('NOR')
+    else:
+        print('MES')
+        cls.append('MES')
+
+df1=df.transpose()
+gs_res = gp.gsea(data = df1,
+                 gene_sets="../signature.gmt",
+                 cls= cls,
+                 # set permutation_type to phenotype if samples >=15
+                 permutation_type = 'phenotype',
+                 permutation_num = 100,
+                 outdir='../Output',
+                 no_plot = False, # Skip plotting
+                 method='signal_to_noise',
+                 processes = 4, seed = 7,
+                 format = 'png')
